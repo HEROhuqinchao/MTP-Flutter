6838fede091fe98c8844692e8dc99689dd85b610
hanasaki <hanasakayui2022@gmail.com>
2025-05-24 15:31:59 +0000

    ✨ 添加flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Flutter";
+
+  inputs = {
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    android-nixpkgs = { url = "github:tadfisher/android-nixpkgs"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat, android-nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk = { accept_license = true; };
+          };
+        };
+        android-sdk = android-nixpkgs.sdk.${system} (sdkPkgs:
+          with sdkPkgs; [
+            cmdline-tools-latest
+            build-tools-34-0-0
+            platform-tools
+            platforms-android-34
+            emulator
+          ]);
+        pinnedJDK = pkgs.jdk;
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            name = "My-flutter-dev-shell";
+            buildInputs = with pkgs; [ flutter ] ++ [ pinnedJDK android-sdk ];
+            JAVA_HOME = pinnedJDK;
+            ANDROID_HOME = "${android-sdk}/share/android-sdk";
+            GRADLE_USER_HOME = "/home/hanasaki/.gradle";
+            GRADLE_OPTS =
+              "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+          };
+        };
+      });
+}
