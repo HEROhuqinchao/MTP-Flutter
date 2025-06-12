93c1ae13c47eec66891355b584de0db4f20c4cac
hanasakayui2022 <hanasakayui2022@gmail.com>
2025-06-12 20:27:19 +0000

    ✨ 添加firebase studio开发环境

---
+++ .idx/dev.nix
@@ -0,0 +1,47 @@
+{pkgs}: {
+  channel = "unstable";
+  packages = [
+    pkgs.jdk17
+    pkgs.unzip
+    pkgs.fish
+    pkgs.starship
+    pkgs.pkg-config
+    pkgs.tree
+  ];
+  idx.extensions = [
+    "dart-code.flutter"
+    "dart-code.dart-code"
+    "vscodevim.vim"
+    "llam4u.nerdtree"
+  ];
+  idx.previews = {
+    previews = {
+      # web = {
+      #   command = [
+      #     "flutter"
+      #     "run"
+      #     "--machine"
+      #     "-d"
+      #     "web-server"
+      #     "--web-hostname"
+      #     "0.0.0.0"
+      #     "--web-port"
+      #     "$PORT"
+      #   ];
+      #   manager = "flutter";
+      # };
+      android = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "android"
+          "-d"
+          "localhost:5555"
+        ];
+        manager = "flutter";
+      };
+    };
+  };
+}
